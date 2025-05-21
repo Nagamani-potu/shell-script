5a17f84b22d7a245e522c1b448ee39ad417d53c1
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-21 15:55:17 +0530

    adding second script

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,8 +2,11 @@
 
 PERSON1=Ramesh
 PERSON2=Suresh
+DATE=$(date)
 
 echo "$PERSON1: Good Morning $PERSON2"
 echo "$PERSON2: Good Morning $PERSON1"
 echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: I am good, What About You $PERSON1"
+
+echo "date and time is : $DATE"
