60f24101b967274ea3f3faaf9035991cb7a69f4a
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-21 16:10:16 +0530

    adding second script

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+# USERNAME=$1
+# PASSWORD=$2
 
-echo "username is : $USERNAME, password is :$PASSWORD"
+# echo "username is : $USERNAME, password is :$PASSWORD"
+
+echo "please engter your username"
+
+read USERNAME #the value entered above is automatically attached to username
+
+echo "username is : $USERNAME, password is $PASSWORD"
