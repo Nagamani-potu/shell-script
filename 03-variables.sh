104b0b5088a19f6b829cdab814947466be174b33
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-21 15:59:49 +0530

    adding second script

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin.bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Good Morning $PERSON2"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good, What About You $PERSON1"
