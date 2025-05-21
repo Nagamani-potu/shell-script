6992d6831b6385ee8cbf78138fbe39424f65f510
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-21 18:07:58 +0530

    adding second script

--- 10-logs.sh
+++ 10-logs.sh
@@ -2,9 +2,10 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-R="\e[31m"
-G="\e[32m"
-N="\e[0m"
+R="\033[31m"  # Red
+G="\033[32m"  # Green
+N="\033[0m"   # Reset
+
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
@@ -12,16 +13,16 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-       echo -e "ERROR:: $2 ...$R failed $N"
+       echo -e "ERROR:: $2 ...${R} failed ${N}"
        exit 1
     else 
-       echo "$2 ...$G success $N"
+       echo "$2 ...${G} success ${N}"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-   echo -e "$R ERROR:: Please run with root access $N"
+   echo -e "${R} ERROR:: Please run with root access ${N}"
    exit 1
 else
    echo "you are a root user"
