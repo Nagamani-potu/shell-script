6a85d908504428247d950a77cfd19b0da95ba8cb
Nagamani-potu <nagamanikp32@gmail.com>
2025-05-21 17:12:42 +0530

    adding second script

---
+++ 09-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(ID -U)
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then 
+       echo "ERROR:: Installing mysql is failed"
+       exit 1
+    else 
+       echo "Mysql installation is success"
+    fi
+}
+
+if [ $? -ne 0 ]
+then
+   echo "ERROR:: Please run with root access"
+   exit 1
+else
+   echo "you are a root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
+
+yum install git -y
+
+VALIDATE
